32dd218a5c2f1af0b5437e949724fd2740c7e694
Connor <90352499+Connor-vb@users.noreply.github.com>
2021-11-27 17:45:46 -0800

    add aboility to add a task

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// creates a list item with the same style through the id assignment
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+};
+
+buttonEl.addEventListener("click", createTaskHandler);
+
+
