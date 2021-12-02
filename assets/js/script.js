769c115b5d3be952773d65ec7993269414957931
Connor <90352499+Connor-vb@users.noreply.github.com>
2021-12-02 13:51:43 -0800

    created a submit event & prevented from refreshing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
 // creates a list item with the same style through the id assignment
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
 
